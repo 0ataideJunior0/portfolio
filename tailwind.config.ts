eec634d21b44a8f3d93605bba52bce1b9b592a6b
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-10-29 15:37:13 -0300

    feat: set colorsTheme in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,17 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      colors:{
+        text: "var(--color-textW)",
+        primary: "var(--color-primary)",
+        background: "var(--color-background)",
+        whiteP: "var(--color-whiteP)",
+        whiteS: "var(--color-whiteS)"
+      }
     },
   },
   plugins: [],
