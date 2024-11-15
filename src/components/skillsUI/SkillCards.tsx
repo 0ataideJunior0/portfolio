803f596564f23dcc3646ed24537b212f39db6881
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-11-15 19:48:27 -0300

    Feat: Change logical structure and improve code layout

--- src/components/skillsUI/SkillCards.tsx
+++ src/components/skillsUI/SkillCards.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import {
   IoBulbOutline,
   IoCodeSlash,
@@ -5,43 +6,58 @@ import {
   IoPlanetOutline,
 } from "react-icons/io5";
 
-export const UxCard = () => {
+interface ICardProps {
+  children: ReactNode;
+  label: string;
+  textSize?: "sm" | "lg" | "md";
+}
+
+export const Card = ({ children, label, textSize = "lg" }: ICardProps) => {
+  const text =
+    textSize === "lg"
+      ? "lg:text-4xl md:text-2xl sm:text-xl"
+      : textSize === "md"
+      ? "lg:text-3xl md:text-2xl sm:text-xl"
+      : "lg:text-xl md:text-xl sm:text-base";
   return (
     <div className="flex flex-col justify-center items-center">
-      <span className="text-whiteP lg:text-4xl sm:text-xl">
-        <IoFingerPrint />
-      </span>
-      <span className="text-whiteP lg:text-4xl sm:text-xl">UX Designer</span>
+      <span className={`text-whiteP ${text}`}>{children}</span>
+      <span className={`text-whiteP ${text}`}>{label}</span>
     </div>
   );
 };
-export const UiCard = () => {
+
+export interface CustomCard {
+  textSize?: "sm" | "lg" | "md";
+}
+export const UxCard = ({ textSize }: CustomCard) => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <span className="text-whiteP lg:text-4xl sm:text-xl">
-        <IoPlanetOutline />
-      </span>
-      <span className="text-whiteP lg:text-4xl sm:text-xl">UI Designer</span>
-    </div>
+    <Card label="UX Designer" textSize={textSize}>
+      <IoFingerPrint />
+    </Card>
   );
 };
-export const SmidCard = () => {
+
+export const UiCard = ({ textSize }: CustomCard) => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <span className="text-whiteP lg:text-4xl sm:text-xl">
-        <IoBulbOutline />
-      </span>
-      <span className="text-whiteP lg:text-4xl sm:text-xl">Social Midia</span>
-    </div>
+    <Card label="UI Designer" textSize={textSize}>
+      <IoPlanetOutline />
+    </Card>
   );
 };
-export const FrontEndCard = () => {
+
+export const SmidCard = ({ textSize }: CustomCard) => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <span className="text-whiteP lg:text-4xl sm:text-xl">
-        <IoCodeSlash />
-      </span>
-      <span className="text-whiteP lg:text-4xl sm:text-xl">Dev FrontEnd</span>
-    </div>
+    <Card label="Social Midia" textSize={textSize}>
+      <IoBulbOutline />
+    </Card>
+  );
+};
+
+export const FrontEndCard = ({ textSize }: CustomCard) => {
+  return (
+    <Card label="Dev FrontEnd" textSize={textSize}>
+      <IoCodeSlash />
+    </Card>
   );
 };
