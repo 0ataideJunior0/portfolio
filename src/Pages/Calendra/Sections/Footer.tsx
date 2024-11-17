777b5bd8d871dbd8e1064acab9cd35ddb7bfe846
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-11-17 20:00:03 -0300

    Feat: Create new section for Calendra page

---
+++ src/Pages/Calendra/Sections/Footer.tsx
@@ -0,0 +1,37 @@
+import logo from "@/assets/images/logo.png";
+import { Socials } from "@/components/Socials";
+import Image from "next/image";
+import Link from "next/link";
+
+export const FooterSection = () => {
+  return (
+    <div className="container py-10">
+      <section className="flex sm:flex-col lg:flex-row lg:gap-8 justify-center dm:gap-8 py-4 items-center">
+        <div
+          className="
+        sm:flex sm:flex-col sm:justify-center gap-2 sm:items-center
+        lg:flex lg:flex-row
+        "
+        >
+          <Link
+            className=" hover:scale-110  transition duration-300"
+            href="/#hero"
+          >
+            <Image src={logo} width={65} height={65} alt="minha logo" />
+          </Link>
+          <span className="text-primary text-base font-semibold items-center">
+            Ataíde Junior
+            <br />
+            UI/UX Designer
+          </span>
+        </div>
+        <div>
+          <Socials />
+        </div>
+      </section>
+      <div className="flex justify-center text-xl text-whiteP mb-4">
+        <span>@2024 Todos os direitos reservados</span>
+      </div>
+    </div>
+  );
+};
---
+++ src/Pages/Calendra/Sections/HeaderNav.tsx
@@ -0,0 +1,105 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { GrTechnology } from "react-icons/gr";
+import { IoMenu } from "react-icons/io5";
+import { RxPaperPlane } from "react-icons/rx";
+import { TbHome, TbScreenShare } from "react-icons/tb";
+
+import logo from "@/assets/images/logo.png";
+import logoName from "@/assets/images/logoName.svg";
+import Link from "next/link";
+
+export const HeaderNavSection = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const togleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); //!false = true
+  };
+  console.log(isMenuOpen);
+  return (
+    <nav
+      className="fixed  top-1 w-full z-30 shadow-md
+      backdrop-blur-md"
+    >
+      <div
+        className="flex items-center lg:justify-evenly sm:justify-between
+        my-2 p-2 mx-4 "
+      >
+        {/* logos */}
+        <Link
+          className=" hover:scale-110  transition duration-300"
+          href="/#hero"
+        >
+          <Image src={logo} width={55} height={55} alt="minha logo" />
+        </Link>
+        <Link
+          className="sm:hidden md:hidden lg:flex hover:scale-110 transition duration-300"
+          href="/#hero"
+        >
+          <Image src={logoName} width={120} height={120} alt="minha logo" />
+        </Link>
+
+        {/* hamburguer menu */}
+        <div
+          onClick={togleMenu}
+          className="lg:hidden text-3xl text-whiteP cursor-pointer"
+        >
+          <IoMenu />
+        </div>
+
+        <ul className="hidden md:hidden lg:flex space-x-4">
+          <li>
+            <a href="#hero" className="nav-item gap-1 flex items-center">
+              Inicio
+              <TbHome />
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="nav-item gap-1 flex items-center">
+              Habilidades
+              <GrTechnology />
+            </a>
+          </li>
+          <li>
+            <a href="#works" className="nav-item gap-1 flex items-center">
+              Trabalhos
+              <TbScreenShare />
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="nav-item gap-1 flex items-center">
+              Contato
+              <RxPaperPlane />
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* mobile menu */}
+      {isMenuOpen ? (
+        <ul className="flex flex-col my-2 justify-center items-center space-y-4 lg:hidden">
+          <li>
+            <a href="#hero" className="nav-item">
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="nav-item">
+              Habilidades
+            </a>
+          </li>
+          <li>
+            <a href="#works" className="nav-item">
+              Trabalhos
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="nav-item">
+              Contato
+            </a>
+          </li>
+        </ul>
+      ) : null}
+    </nav>
+  );
+};
